69714f3e6aeadcf92a929c957de750956d25ef1c
Gabriel Cohen <glcohen@ucsd.edu>
2016-08-27 15:32:09 -0700

    trying to allow more youtube videos to play

--- js/services/search.js
+++ js/services/search.js
@@ -15,7 +15,8 @@
                     'q' : query,
                     'type':'video',
                     'videoEmbeddable': 'true',
-                    //Sharing this key in the hopes that it wont be abused 
+                    'regionCode' : 'US',
+                    //Sharing this key in the hopes that it wont be abused
                     'key': config.youtube.key
                 }
             });
