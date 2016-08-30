09900165d266d452111ed9b2fd04e4d3c6db63f0
Gabriel Cohen <glcohen@ucsd.edu>
2016-08-29 23:00:15 -0700

    Testing artyom with electron

--- js/services/time.js
+++ js/services/time.js
@@ -1,38 +1,39 @@
-(function() {
-  'use strict';
+  (function() {
+    'use strict';
 
-  function getTime() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var t = "The time is ";
-    if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-    if (hours > 11) {
-      if (hours != 12) {
-        hours = hours - 12;
+    function getTime() {
+      var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var minutes = currentTime.getMinutes();
+      var t = "The time is ";
+      if (minutes < 10) {
+        minutes = "0" + minutes;
       }
-      t += hours + ":" + minutes + " " + "PM";
+      if (hours > 11) {
+        if (hours != 12) {
+          hours = hours - 12;
+        }
+        t += hours + ":" + minutes + " " + "PM";
+      }
+      if (hours <= 11) {
+        t += hours + ":" + minutes + " " + "AM";
+      }
+
+      return t;
     }
-    if (hours <= 11) {
-      t += hours + ":" + minutes + " " + "AM";
+
+    function TimeService() {
+        var service = {};
+
+        service.speakTime = function(msg) {
+          // var msg = new SpeechSynthesisUtterance(getTime());
+          // speechSynthesis.speak(msg);
+          artyom.say(getTime());
+        };
+
+        return service;
     }
 
-    return t;
-  }
-
-  function TimeService() {
-      var service = {};
-
-      service.speakTime = function(msg) {
-        var msg = new SpeechSynthesisUtterance(getTime());
-        speechSynthesis.speak(msg);
-      };
-
-      return service;
-  }
-
-  angular.module('SmartMirror')
-    .factory('TimeService', TimeService);
-}());
+    angular.module('SmartMirror')
+      .factory('TimeService', TimeService);
+  }());
