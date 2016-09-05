4cf01c9c5575d0bf25bb7b90d84fc1614fca3583
Gabriel Cohen <glcohen@ucsd.edu>
2016-09-05 16:58:23 -0700

    trying google speech api fix

--- js/services/time.js
+++ js/services/time.js
@@ -23,13 +23,37 @@
       return t;
     }
 
+
     function TimeService() {
         var service = {};
-        var vocal = require(__dirname + "/node_modules/vocal/lib/vocal.js");
-        service.speakTime = function(msg) {
-          vocal("Testing 1 2 3 Testing One Two Three.. Is this thing on?");
-          console.log("\"" + getTime() + '\" has been spoken');
+        // var vocal = require(__dirname + "/node_modules/vocal/lib/vocal.js");
+        // service.speakTime = function(msg) {
+        //   vocal("Testing 1 2 3 Testing One Two Three.. Is this thing on?");
+        //   console.log("\"" + getTime() + '\" has been spoken');
+        // };
+        //Declare the speech object & set attributes
+        var utterance = new window.SpeechSynthesisUtterance();
+        utterance.lang = 'en-US';
+        utterance.volume = 1.0;
+        utterance.rate = 1.0;
+        utterance.pitch = 1.0;
+        utterance.text = "I'm a working voice, oh shit!";
+
+        //Speak the phrase
+        window.speechSynthesis.speak(utterance);
+
+        window.speechSynthesis.onvoiceschanged = function () {
+          var speechSynthesisVoices = speechSynthesis.getVoices();
+          var accents = _(speechSynthesisVoices).pluck('lang');
+          var voices = _(speechSynthesisVoices).pluck('voiceURI');
+          var names = _(speechSynthesisVoices).pluck('name');
+          console.log('names', names);
+          console.log('accents', _.uniq(accents));
+          console.log('voices', voices);
         };
+
+        // var msg = new SpeechSynthesisUtterance(getTime());
+        // speechSynthesis.speak(msg);
         return service;
     }
 
