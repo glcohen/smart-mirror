8e9728dadd1f6a45c6314cd5767802de1674c658
Gabriel Cohen <glcohen@ucsd.edu>
2016-10-08 15:21:59 -0700

    more bugs

--- js/services/time.js
+++ js/services/time.js
@@ -24,11 +24,11 @@
     }
 
 
-    function TimeService() {
+    function TimeService(vocal) {
         var service = {};
         //var vocal = require(__dirname + "/node_modules/vocal/lib/vocal.js");
         service.speakTime = function(msg) {
-          vocal("Testing 1 2 3 Testing One Two Three.. Is this thing on?");
+          vocal("The time is" + getTime());
           console.log("\"" + getTime() + '\" has been spoken');
         };
 
